b09810c85584f27b63583113f0c8f4e32d8e93c9
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-30 12:01:58 +0200

    Commit

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 console.log("axios", axios)
 
-axios.get('https://pokeapi.co/api/v2/pokemon/1')
-.then(response => {
-  console.log(response.data);
-  let name = response.data.name
-  let baseExperience = response.data.base_experience
-  let picture = response.data.sprites.front_default;
-
-  console.log(name, baseExperience, picture);
-
-  $('.player-1 .pokemon-info').html(`
-    <img src="${picture}" />
-  `)
-  
+$('.player-1 button').click(e => {
+  e.preventDefault()
+  let val = $('.player-1 input').val()
+  callPokeApi(val, 1)
 })
+
+$('.player-2 button').click(e => {
+  e.preventDefault()
+  let val = $('.player-2 input').val()
+  callPokeApi(val, 2)
+})
+
+function callPokeApi(val, playerId) {
+  axios.get('https://pokeapi.co/api/v2/pokemon/'+val)
+  .then(response => {
+    console.log(response.data);
+    let name = response.data.name
+    let baseExperience = response.data.base_experience
+    let picture = response.data.sprites.front_default;
+  
+    console.log(name, baseExperience, picture);
+  
+    $(`.player-${playerId} .pokemon-info`).html(`
+      <img class="poke-picture" src="${picture}" />
+      <h3>${name}</h3>
+      <h4>${baseExperience}</h4>
+    `)
+  })
+}
+
+
