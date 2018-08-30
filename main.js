bfce28a473e6382f1f9c1d916966bcc9ac992963
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-30 12:20:00 +0200

    Fight ready

--- main.js
+++ main.js
@@ -24,10 +24,32 @@ function callPokeApi(val, playerId) {
   
     $(`.player-${playerId} .pokemon-info`).html(`
       <img class="poke-picture" src="${picture}" />
-      <h3>${name}</h3>
-      <h4>${baseExperience}</h4>
+      <h3 class="name">${name}</h3>
+      <h4 class="base-experience">${baseExperience}</h4>
     `)
   })
 }
 
+$('.fight-button').click(() => {
+  let baseExperience1 = Number($('.player-1 .base-experience').text())
+  let baseExperience2 = Number($('.player-2 .base-experience').text())
+  console.log('DEBUG baseExperience1', baseExperience1);
+  console.log('DEBUG baseExperience2', baseExperience2);
+
+  if (baseExperience1 > 0 && baseExperience2 > 0) {
+    let winnerId, loserId;
+    if (Math.random() < baseExperience1 / (baseExperience1 + baseExperience2)) {
+      winnerId = 1
+      loserId = 2
+    }
+    else {
+      winnerId = 2
+      loserId = 1
+    }
+    let winnerName = $(`.player-${winnerId} .name`).text()
+
+    $('.winner').text('The winner is ' + winnerName)
+    $(`.player-${loserId} .pokemon-info`).html('')
+  }
+})
 
