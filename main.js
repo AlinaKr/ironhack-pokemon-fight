54c16a43b9a03b14e33e08043973d81592f8810d
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-30 11:39:10 +0200

    Improvement of design

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("axios", axios)
 
-axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+axios.get('https://pokeapi.co/api/v2/pokemon/1')
 .then(response => {
   console.log(response.data);
   let name = response.data.name
