68529a73f54260ffb3daa1b8a967785d661464bf
Maxence Bouret <maxence.bouret@gmail.com>
2018-09-01 14:30:41 +0200

    Use of own data

---
+++ README.md
@@ -0,0 +1,3 @@
+# Info
+
+The source for `data/pokemon.json` comes from https://github.com/PokeAPI/pokeapi/blob/master/data/v2/csv/pokemon.csv
--- main.js
+++ main.js
@@ -1,59 +1,123 @@
-console.log("axios", axios)
+function capitalze(str) {
+  return str[0].toUpperCase() + str.substr(1).toLowerCase()
+}
+
+let pokemonsOnField = new Array(2)
+let nbOfPokeballs = [6,6]
+
+displayNbOfPokeballs()
+
 
 $('.player-1 button').click(e => {
   e.preventDefault()
   let val = $('.player-1 input').val()
-  callPokeApi(val, 1)
+  $('.player-1 input').val('')
+  summonPokemon(val, 1)
 })
 
 $('.player-2 button').click(e => {
   e.preventDefault()
   let val = $('.player-2 input').val()
-  callPokeApi(val, 2)
+  $('.player-2 input').val('')
+  summonPokemon(val, 2)
 })
 
-function callPokeApi(val, playerId) {
-  $(`.player-${playerId} .pokemon-info`).html(`
-      <h3 class="my-5">Loading...</h3>
-    `)
+function summonPokemon(val, playerNb) {
+  $('.winner').html('')
+  $(`.player-${playerNb} .pokemon-info`).html(`
+    <h3 class="my-5"><img src="images/poke-ball.png"></h3>
+  `)
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/'+val)
+  nbOfPokeballs[playerNb-1]--
+  displayNbOfPokeballs()
+
+  
+  axios.get('data/pokemon.json')
   .then(response => {
-    console.log(response.data);
-    let name = response.data.name
-    let baseExperience = response.data.base_experience
-    let picture = response.data.sprites.front_default;
+    let pokemon = response.data.find(p => p.species_id === Number(val) || p.identifier === val.toLowerCase().trim())
+    console.log('DEBUG pokemon', pokemon);
+    if (!pokemon) {
+      pokemon = response.data[Math.floor(Math.random() * 718)]
+    }
+    
+    console.log('DEBUG pokemon', pokemon);
+
+    let name = capitalze(pokemon.identifier)
+    let baseExperience = pokemon.base_experience
+    let picture = `images/pokemons/${pokemon.species_id}.png`
+    let life = 100
+
+    pokemonsOnField[playerNb-1] = { name, baseExperience, picture, life }
   
     console.log(name, baseExperience, picture);
-  
-    $(`.player-${playerId} .pokemon-info`).html(`
-      <img class="poke-picture" src="${picture}" />
-      <h3 class="name">${name}</h3>
-      <h4 class="base-experience">${baseExperience}</h4>
-    `)
+    
+    setTimeout(() => {
+      displayPokemon(playerNb-1)
+    }, 2000)
   })
 }
 
 $('.fight-button').click(() => {
-  let baseExperience1 = Number($('.player-1 .base-experience').text())
-  let baseExperience2 = Number($('.player-2 .base-experience').text())
-  console.log('DEBUG baseExperience1', baseExperience1);
-  console.log('DEBUG baseExperience2', baseExperience2);
+  if (!pokemonsOnField[0] || !pokemonsOnField[1]) return;
 
-  if (baseExperience1 > 0 && baseExperience2 > 0) {
-    let winnerId, loserId;
-    if (Math.random() < baseExperience1*baseExperience1 / (baseExperience1*baseExperience1 + baseExperience2*baseExperience2)) {
-      winnerId = 1
-      loserId = 2
+  let intervalId = setInterval(() => {
+    let baseExperience1 = pokemonsOnField[0].baseExperience
+    let baseExperience2 = pokemonsOnField[1].baseExperience
+  
+    if (baseExperience1 > 0 && baseExperience2 > 0) {
+      let winnerId, loserId;
+      if (Math.random() < baseExperience1*baseExperience1 / (baseExperience1*baseExperience1 + baseExperience2*baseExperience2)) {
+        winnerId = 0
+        loserId = 1
+      }
+      else {
+        winnerId = 1
+        loserId = 0
+      }
+  
+      pokemonsOnField[loserId].life -= 10
+  
+      if (pokemonsOnField[loserId].life > 0) {
+        displayPokemon(loserId)
+      }
+      else {
+        displayWinner(winnerId, loserId)
+        pokemonsOnField[loserId] = undefined
+        displayNbOfPokeballs()
+        clearInterval(intervalId)
+      }
     }
-    else {
-      winnerId = 2
-      loserId = 1
-    }
-    let winnerName = $(`.player-${winnerId} .name`).text()
-
-    $('.winner').text('The winner is ' + winnerName)
-    $(`.player-${loserId} .pokemon-info`).html('')
-  }
+  }, 200)
 })
 
+
+function displayPokemon(playerId) {
+  let { name, picture, baseExperience, life } = pokemonsOnField[playerId]
+  $(`.player-${playerId+1} .pokemon-info`).html(`
+    <img class="poke-picture" src="${picture}" />
+    <h3 class="name">${name}</h3>
+    <h4 class="base-experience">${baseExperience}</h4>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" style="width: ${life}%" aria-valuenow="${life}" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+  `)
+}
+
+function displayWinner(winnerId, loserId) {
+  let winnerName = pokemonsOnField[winnerId].name
+
+  $('.winner').text('The winner is ' + winnerName)
+  $(`.player-${loserId+1} .pokemon-info`).html('')
+}
+
+function displayNbOfPokeballs() {
+  $('.player-1 .score').html('')
+  for (let i = 0; i < nbOfPokeballs[0]; i++) {
+    $('.player-1 .score').append(`<img src="images/poke-ball.png">`)
+  }
+
+  $('.player-2 .score').html('')
+  for (let i = 0; i < nbOfPokeballs[1]; i++) {
+    $('.player-2 .score').append(`<img src="images/poke-ball.png">`)
+  }
+} 
